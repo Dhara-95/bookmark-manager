30dfef992ba338c011871c97646100cccc647200
Dhara <dhara.patel95@hotmail.co.uk>
2020-07-03 12:07:06 +0100

    Step 10 complete - functionality of adding bookmarks working and tests passed

--- bookmark-manager/spec/bookmark_spec.rb
+++ bookmark-manager/spec/bookmark_spec.rb
@@ -2,7 +2,7 @@ require './lib/bookmark.rb'
 
 describe Bookmark do
   
-  describe '#.all' do
+  describe '#all' do
     it 'returns a list of all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
@@ -15,6 +15,13 @@ describe Bookmark do
       expect(bookmarks).to include('http://www.makersacademy.com')
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com')
+    end
+  end
+
+  describe '#create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.twitter.com')
+      expect(Bookmark.all).to include 'http://www.twitter.com'
     end
   end
 end
--- bookmark-manager/spec/features/viewing_bookmarks_spec.rb
+++ bookmark-manager/spec/features/viewing_bookmarks_spec.rb
@@ -1,20 +1,24 @@
 feature 'Viewing bookmarks' do
-  scenario 'visiting the homepage page' do
-    visit('/')
-    expect(page).to have_content("Bookmark Manager")
+
+
+  feature 'visiting the homepage' do
+    scenario 'title of app is shown' do
+      visit('/')
+      expect(page).to have_content("Bookmark Manager")
+    end
   end
+  
+  feature 'viewing bookmarks' do
+    scenario 'bookmarks are visible to user' do
+      Bookmark.create(url: "http://www.makersacademy.com")
+      Bookmark.create(url: "http://www.destroyallsoftware.com")
+      Bookmark.create(url: "http://www.google.com")
 
-  scenario 'bookmarks are visible to user' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
+      visit('/bookmarks')
 
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-
-    visit('/bookmarks')
-
-    expect(page).to have_content("http://www.makersacademy.com")
-    expect(page).to have_content("http://www.destroyallsoftware.com")
-    expect(page).to have_content("http://www.google.com")
+      expect(page).to have_content("http://www.makersacademy.com")
+      expect(page).to have_content("http://www.destroyallsoftware.com")
+      expect(page).to have_content("http://www.google.com")
+    end
   end
 end
